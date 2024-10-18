e2a7b900a3d402ed098d9fc1b919edf8ee26c3f1
Alina Strashevichutse <astrashevichutse@exadel.com>
2024-10-18 18:08:59 +0200

    rewrite test with PO methods

--- test/search_flight_test.py
+++ test/search_flight_test.py
@@ -1,41 +1,29 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from pytest import fixture
+from page_objects.searchBar import SearchBar
+import time
 
 
-def main():
-    driver = set_up_driver()
-    driver.get('https://www.expedia.com/Flights')
-    search_test(driver)
-
-
-def set_up_driver():
+@fixture
+def driver():
     driver_path = ChromeDriverManager().install()
     driver = webdriver.Chrome(service=ChromeService(driver_path))
     driver.maximize_window()
     return driver
 
 
-def search_test(driver):
-    origin_btn = driver.find_element(By.CSS_SELECTOR, "button[data-stid='origin_select-menu-trigger']")
-    origin_btn.click()
-    WebDriverWait(driver, 5).until(
-        EC.visibility_of_element_located((By.CSS_SELECTOR, "section[data-testid='popover-sheet']")))
-    inpt = driver.find_element(By.CSS_SELECTOR, "input[data-stid='origin_select-menu-input']")
-    inpt.send_keys("Orlando, FL (MCO)")
-    WebDriverWait(driver, 5).until(
-        EC.visibility_of_element_located((By.CSS_SELECTOR, "ul[data-testid='origin_select-results']")))
+def test_search(driver):
+    driver.get('https://www.expedia.com/Flights')
+    search_bar = SearchBar(driver)
+    search_bar.activate_leaving_field()
+    search_bar.wait_for_popover()
+    search_bar.type_origin_destination("Orlando, FL (MCO)")
+    #search_bar.wait_for_search_results()
+    time.sleep(100)
     # test fails here, because options list infinity loadings due to selenium detection on the original site.
-    city_origin = driver.find_element(By.CSS_SELECTOR,
-                                      "button[aria-label='Orlando (MCO - Orlando Intl.) Florida, United States'")
-    city_origin.click()
-    origin_btn_text = driver.find_element(By.CSS_SELECTOR, "button[data-stid='origin_select-menu-input']").text
-    assert origin_btn_text == "Orlando, FL (MCO)"
+    search_bar.choose_origin_city("Orlando, FL (MCO)")
     # I stopped to create the test here, because the website detected and blocked Selenium actions.
 
 
-if __name__ == "__main__":
-    main()
